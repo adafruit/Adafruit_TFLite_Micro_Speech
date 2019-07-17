9bd89c78cd38b09f3f5140acb071f942b5843376
ladyada <limor@ladyada.net>
2019-07-17 12:02:28 -0400

    implement https://github.com/adafruit/Adafruit_TFLite_Micro_Speech/issues/1

--- src/tensorflow/lite/experimental/micro/kernels/depthwise_conv.cpp
+++ src/tensorflow/lite/experimental/micro/kernels/depthwise_conv.cpp
@@ -35,6 +35,9 @@ constexpr int kFilterTensor = 1;
 constexpr int kBiasTensor = 2;
 constexpr int kOutputTensor = 0;
 
+// Size of the cached buffer we'll be using to hold reordered weights.
+constexpr int kReshapedFilterDataSize = 1 * 1024;
+
 struct OpData {
   TfLitePaddingValues padding;
   // The scaling factor from input to output (aka the 'real multiplier') can
@@ -50,12 +53,12 @@ struct OpData {
 TfLiteStatus CalculateOpData(TfLiteContext* context, TfLiteNode* node,
                              TfLiteDepthwiseConvParams* params, int width,
                              int height, int filter_width, int filter_height,
+                             int out_width, int out_height,
                              const TfLiteType data_type, OpData* data) {
-  int unused_output_height, unused_output_width;
-  data->padding = ComputePaddingHeightWidth(
-      params->stride_height, params->stride_width, 1, 1, height, width,
-      filter_height, filter_width, params->padding, &unused_output_height,
-      &unused_output_width);
+  data->padding.height = ComputePadding(params->stride_height, 1, height,
+                                        filter_height, out_height);
+  data->padding.width =
+      ComputePadding(params->stride_width, 1, width, filter_width, out_width);
 
   // Note that quantized inference requires that all tensors have their
   // parameters set. This is usually done during quantized training.
@@ -78,6 +81,194 @@ TfLiteStatus CalculateOpData(TfLiteContext* context, TfLiteNode* node,
   }
   return kTfLiteOk;
 }
+
+// Specialized implementation of the depthwise convolution operation designed to
+// work with the particular filter width of eight used by the default micro
+// speech sample code. It uses 1KB of RAM to hold reordered weight parameters,
+// converted from TFLite's NHWC format to NCHW format, and expressed as signed
+// eight bit integers, rather than unsigned. Care must be taken when calling
+// this not to use it for more than one node since there's only a single static
+// buffer holding the weights. You should use this implementation if depthwise
+// convolutions are a performance bottleneck, you have a layer that meets the
+// parameter requirements, and the extra RAM usage and additional code size are
+// not an issue.
+static inline void DepthwiseConvOptimizedForFilterWidthEight(
+    TfLiteContext* context, const DepthwiseParams& params,
+    const RuntimeShape& input_shape, const uint8* input_data,
+    const RuntimeShape& filter_shape, const uint8* filter_data,
+    const RuntimeShape& bias_shape, const int32* bias_data,
+    const RuntimeShape& output_shape, uint8* output_data) {
+  const int stride_width = params.stride_width;
+  const int stride_height = params.stride_height;
+  const int pad_width = params.padding_values.width;
+  const int pad_height = params.padding_values.height;
+  const int depth_multiplier = params.depth_multiplier;
+  const int32 output_activation_min = params.quantized_activation_min;
+  const int32 output_activation_max = params.quantized_activation_max;
+  const int32 input_offset = params.input_offset;
+  const int32 filter_offset = params.weights_offset;
+  const int32 output_offset = params.output_offset;
+  const int32 output_multiplier = params.output_multiplier;
+  const int output_shift = params.output_shift;
+  TFLITE_DCHECK_EQ(input_shape.DimensionsCount(), 4);
+  TFLITE_DCHECK_EQ(filter_shape.DimensionsCount(), 4);
+  TFLITE_DCHECK_EQ(output_shape.DimensionsCount(), 4);
+
+  TFLITE_DCHECK_LE(output_activation_min, output_activation_max);
+  const int batches = MatchingDim(input_shape, 0, output_shape, 0);
+  const int output_depth = MatchingDim(filter_shape, 3, output_shape, 3);
+  const int input_height = input_shape.Dims(1);
+  const int input_width = input_shape.Dims(2);
+  const int input_depth = input_shape.Dims(3);
+  const int filter_height = filter_shape.Dims(1);
+  const int filter_width = filter_shape.Dims(2);
+  const int output_height = output_shape.Dims(1);
+  const int output_width = output_shape.Dims(2);
+  TFLITE_DCHECK_EQ(output_depth, input_depth * depth_multiplier);
+  TFLITE_DCHECK_EQ(bias_shape.FlatSize(), output_depth);
+
+  static int8_t reshaped_filter_data[kReshapedFilterDataSize];
+  const int needed_size =
+      output_depth * filter_width * filter_height * input_depth;
+  if (needed_size > kReshapedFilterDataSize) {
+    context->ReportError(
+        context,
+        "Size too large for reshaped weight buffer (%d needed, %d available)",
+        needed_size, kReshapedFilterDataSize);
+    return;
+  }
+
+  RuntimeShape reshaped_filter_shape;
+  reshaped_filter_shape.BuildFrom(
+      {1, output_depth, filter_height, filter_width});
+
+  // If this is the first time through, repack the weights into a cached buffer
+  // so that they can be accessed sequentially.
+  static bool is_reshaped_filter_initialized = false;
+  if (!is_reshaped_filter_initialized) {
+    for (int filter_y = 0; filter_y < filter_height; ++filter_y) {
+      for (int filter_x = 0; filter_x < filter_width; ++filter_x) {
+        for (int oc = 0; oc < output_depth; ++oc) {
+          const uint8* current_filter =
+              filter_data + Offset(filter_shape, 0, filter_y, filter_x, oc);
+          int8* reshaped_filter =
+              reshaped_filter_data +
+              Offset(reshaped_filter_shape, 0, oc, filter_y, filter_x);
+          *reshaped_filter = (int32_t)(*current_filter) + filter_offset;
+        }
+      }
+    }
+    is_reshaped_filter_initialized = true;
+  }
+
+  for (int b = 0; b < batches; ++b) {
+    for (int out_y = 0; out_y < output_height; ++out_y) {
+      for (int out_x = 0; out_x < output_width; ++out_x) {
+        for (int ic = 0; ic < input_depth; ++ic) {
+          for (int m = 0; m < depth_multiplier; m++) {
+            const int oc = m + ic * depth_multiplier;
+            const int in_x_origin = (out_x * stride_width) - pad_width;
+            const int in_y_origin = (out_y * stride_height) - pad_height;
+            int32 acc = 0;
+            int in_y_start = in_y_origin;
+            int filter_y_start = 0;
+            if (in_y_origin < 0) {
+              in_y_start = 0;
+              filter_y_start = 0 - in_y_origin;
+            }
+            int filter_y_end = filter_height;
+            if ((in_y_origin + filter_height) >= input_height) {
+              filter_y_end -= (in_y_origin + filter_height) - input_height;
+            }
+            int in_y = in_y_start;
+            int in_x_start = in_x_origin;
+            int filter_x_start = 0;
+            bool is_out_of_x_bounds = false;
+            if (in_x_origin < 0) {
+              in_x_start = 0;
+              filter_x_start = 0 - in_x_origin;
+              is_out_of_x_bounds = true;
+            }
+            int filter_x_end = filter_width;
+            if ((in_x_origin + filter_width) >= input_width) {
+              filter_x_end -= (in_x_origin + filter_width) - input_width;
+              is_out_of_x_bounds = true;
+            }
+            for (int filter_y = filter_y_start; filter_y < filter_y_end;
+                 ++filter_y, ++in_y) {
+              const uint8* current_input =
+                  input_data + Offset(input_shape, b, in_y, in_x_start, ic);
+              if ((filter_width == 8) && !is_out_of_x_bounds) {
+                int8* current_filter =
+                    reshaped_filter_data + Offset(reshaped_filter_shape, 0, oc,
+                                                  filter_y, filter_x_start);
+                const uint32_t input_vals0 =
+                    *reinterpret_cast<const uint32_t*>(current_input);
+                current_input += 4;
+                const int32_t filter_vals0 =
+                    *reinterpret_cast<const int32_t*>(current_filter);
+                current_filter += 4;
+                const uint8 input_val0 = input_vals0 & 0xff;
+                const int8 filter_val0 = filter_vals0 & 0xff;
+                acc += filter_val0 * input_val0;
+                const uint8 input_val1 = (input_vals0 >> 8) & 0xff;
+                const int8 filter_val1 = (filter_vals0 >> 8) & 0xff;
+                acc += filter_val1 * input_val1;
+                const uint8 input_val2 = (input_vals0 >> 16) & 0xff;
+                const int8 filter_val2 = (filter_vals0 >> 16) & 0xff;
+                acc += filter_val2 * input_val2;
+                const uint8 input_val3 = (input_vals0 >> 24) & 0xff;
+                const int8 filter_val3 = (filter_vals0 >> 24) & 0xff;
+                acc += filter_val3 * input_val3;
+
+                const uint32_t input_vals1 =
+                    *reinterpret_cast<const uint32_t*>(current_input);
+                const int32_t filter_vals1 =
+                    *reinterpret_cast<const int32_t*>(current_filter);
+                const uint8 input_val4 = input_vals1 & 0xff;
+                const int8 filter_val4 = filter_vals1 & 0xff;
+                acc += filter_val4 * input_val4;
+                const uint8 input_val5 = (input_vals1 >> 8) & 0xff;
+                const int8 filter_val5 = (filter_vals1 >> 8) & 0xff;
+                acc += filter_val5 * input_val5;
+                const uint8 input_val6 = (input_vals1 >> 16) & 0xff;
+                const int8 filter_val6 = (filter_vals1 >> 16) & 0xff;
+                acc += filter_val6 * input_val6;
+                const uint8 input_val7 = (input_vals1 >> 24) & 0xff;
+                const int8 filter_val7 = (filter_vals1 >> 24) & 0xff;
+                acc += filter_val7 * input_val7;
+              } else {
+                const uint8* current_filter =
+                    filter_data +
+                    Offset(filter_shape, 0, filter_y, filter_x_start, oc);
+                for (int filter_x = filter_x_start; filter_x < filter_x_end;
+                     ++filter_x) {
+                  int32 input_val = *current_input;
+                  current_input += input_depth;
+                  int32 filter_val = *current_filter;
+                  current_filter += output_depth;
+                  acc +=
+                      (filter_val + filter_offset) * (input_val + input_offset);
+                }
+              }
+            }
+            if (bias_data) {
+              acc += bias_data[oc];
+            }
+            acc = reference_ops::depthwise_conv::DepthwiseConvRound<
+                DepthwiseConvOutputRounding::kAwayFromZero>(
+                acc, output_multiplier, output_shift);
+            acc += output_offset;
+            acc = std::max(acc, output_activation_min);
+            acc = std::min(acc, output_activation_max);
+            output_data[Offset(output_shape, b, out_y, out_x, oc)] =
+                static_cast<uint8>(acc);
+          }
+        }
+      }
+    }
+  }
+}  // namespace
 
 }  // namespace
 
@@ -146,11 +337,51 @@ void EvalQuantized(TfLiteContext* context, TfLiteNode* node,
   // Legacy ops used mixed left and right shifts. Now all are +ve-means-left.
   op_params.output_shift = -data->output_shift;
 
-  tflite::reference_ops::DepthwiseConv(
-      op_params, GetTensorShape(input), GetTensorData<uint8_t>(input),
-      GetTensorShape(filter), GetTensorData<uint8_t>(filter),
-      GetTensorShape(bias), GetTensorData<int32_t>(bias),
-      GetTensorShape(output), GetTensorData<uint8_t>(output));
+  // Figure out if we can use the optimized path for this set of parameters.
+  const int filter_width = GetTensorShape(filter).Dims(2);
+  const int input_depth = GetTensorShape(input).Dims(3);
+  const int output_depth = GetTensorShape(filter).Dims(3);
+  const int filter_height = GetTensorShape(filter).Dims(1);
+  const int needed_size =
+      output_depth * filter_width * filter_height * input_depth;
+  bool use_optimized_path = false;
+  if ((filter_width == 8) && (input_offset == 0) && (filter_offset == -127) &&
+      (input_depth == 1) && (needed_size <= kReshapedFilterDataSize)) {
+    // FIXME(petewarden) - We need a more robust way of handling this, ideally
+    // with an allocation mechanism available through the context API.
+    // Use the address of the node as a proxy for its identity, since we need
+    // to ensure the weight values are consistent between calls, and there's
+    // no easy way to do that quickly other than relying on the identity of
+    // the owning node.
+    static TfLiteNode* initialized_node_address = node;
+    if (initialized_node_address == node) {
+      use_optimized_path = true;
+    } else {
+      static bool has_warned = false;
+      if (!has_warned) {
+        context->ReportError(
+            context,
+            "Multiple depthwise conv ops match optimization parameters, but "
+            "only the first will use the fast path, because there's only one "
+            "RAM cache available");
+        has_warned = true;
+      }
+    }
+  }
+  if (use_optimized_path) {
+    DepthwiseConvOptimizedForFilterWidthEight(
+        context, op_params, GetTensorShape(input),
+        GetTensorData<uint8_t>(input), GetTensorShape(filter),
+        GetTensorData<uint8_t>(filter), GetTensorShape(bias),
+        GetTensorData<int32_t>(bias), GetTensorShape(output),
+        GetTensorData<uint8_t>(output));
+  } else {
+    tflite::reference_ops::DepthwiseConv(
+        op_params, GetTensorShape(input), GetTensorData<uint8_t>(input),
+        GetTensorShape(filter), GetTensorData<uint8_t>(filter),
+        GetTensorShape(bias), GetTensorData<int32_t>(bias),
+        GetTensorShape(output), GetTensorData<uint8_t>(output));
+  }
 }
 
 TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
@@ -168,11 +399,15 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   int height = SizeOfDimension(input, 1);
   int filter_width = SizeOfDimension(filter, 2);
   int filter_height = SizeOfDimension(filter, 1);
+  int out_width = ComputeOutSize(params->padding, width, filter_width,
+                                 params->stride_width);
+  int out_height = ComputeOutSize(params->padding, height, filter_height,
+                                  params->stride_height);
   OpData local_data_object;
   OpData* data = &local_data_object;
   TF_LITE_ENSURE_STATUS(CalculateOpData(context, node, params, width, height,
-                                        filter_width, filter_height, data_type,
-                                        data));
+                                        filter_width, filter_height, out_width,
+                                        out_height, data_type, data));
 
   // TODO(aselle): Consider whether float conv and quantized conv should be
   // separate ops to avoid dispatch overhead here.
