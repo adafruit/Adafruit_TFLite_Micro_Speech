a8da9bc09c9e68ab330cb03642813d22a54b276d
ladyada <limor@ladyada.net>
2019-10-10 23:56:17 -0400

    generalized

--- examples/rawaudio_play/rawaudio_play.ino
+++ examples/rawaudio_play/rawaudio_play.ino
@@ -2,13 +2,14 @@
 #include "yes.h"
 #include "no.h"
 
+// Simple DAC-output audio player for recorded samples, good for testing!
+#define AUDIO_OUT A0
 
 void setup() {
   Serial.begin(115200);
   
   analogWriteResolution(12);
-  analogWrite(A0, 128);
-  analogWrite(A1, 128);
+  analogWrite(AUDIO_OUT, 128);
 
   play_tune_i16(g_yes_1000ms_sample_data, g_yes_1000ms_sample_data_size);
   delay(100);
@@ -25,8 +26,7 @@ void play_tune(const uint8_t *audio, uint32_t audio_length) {
   
   for (uint32_t i=0; i<audio_length; i++) {
     while((t = micros()) - prior < usec);
-    analogWrite(A0, (uint16_t)audio[i] / 8);
-    analogWrite(A1, (uint16_t)audio[i] / 8);
+    analogWrite(AUDIO_OUT, (uint16_t)audio[i] / 8);
     prior = t;
   }
 }
@@ -39,7 +39,7 @@ void play_tune_i16(const int16_t *audio, uint32_t audio_length) {
   
   for (uint32_t i=0; i<audio_length; i++) {
     while((t = micros()) - prior < usec);
-    analogWrite(A0, (audio[i]+32767) / 16);
+    analogWrite(AUDIO_OUT, (audio[i]+32767) / 16);
     prior = t;
   }
 }
