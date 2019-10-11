578f3b7346d4525b6fa29b124797397224c84bef
ladyada <limor@ladyada.net>
2019-10-11 00:17:23 -0400

    unify pdmmic + analogmic

--- examples/rawaudio_recplay/rawaudio_recplay.ino
+++ examples/rawaudio_recplay/rawaudio_recplay.ino
@@ -1,28 +1,32 @@
-#define USE_EXTERNAL_MIC A8  // D2 on pygbadge
-//#define USE_EDGEBADGE_PDMMIC
+//#define USE_EXTERNAL_MIC A8  // D2 on pygbadge
+#define USE_EDGEBADGE_PDMMIC
 
 #include <SPI.h>
-#include "Adafruit_ZeroTimer.h"
 #include "Adafruit_NeoPixel.h"
 
 #define LED_OUT       LED_BUILTIN
 #define BUTTON        12
 #define NEOPIXEL_PIN  8
-
 #define AUDIO_OUT     A0
+
 #if defined(USE_EXTERNAL_MIC)
-  #define AUDIO_IN      USE_EXTERNAL_MIC
+  #include "Adafruit_ZeroTimer.h"
+  #define AUDIO_IN         USE_EXTERNAL_MIC
   #define DAC_TIMER        5
   void TC5_Handler(){
     Adafruit_ZeroTimer::timerHandler(DAC_TIMER);
   }
   Adafruit_ZeroTimer zt = Adafruit_ZeroTimer(DAC_TIMER);
 #endif
-
+#if defined(USE_EDGEBADGE_PDMMIC)
+  #include <Adafruit_ZeroPDMSPI.h>
+  #define PDM_SPI            SPI2    // PDM mic SPI peripheral
+  #define TIMER_CALLBACK     SERCOM3_0_Handler
+  Adafruit_ZeroPDMSPI pdmspi(&PDM_SPI);
+#endif
 
 #define SAMPLERATE_HZ 16000
 #define BUFFER_SIZE   SAMPLERATE_HZ
-
 volatile uint16_t audio_idx = 0;
 uint16_t audio_max = 0;
 int16_t audio_buffer[BUFFER_SIZE];
@@ -33,14 +37,30 @@ volatile bool val;
 volatile bool isPlaying, isRecording;
 bool button_state;
 
-void TimerCallback()
+void TIMER_CALLBACK()
 {
+  int32_t sample;
+
+#if defined(USE_EDGEBADGE_PDMMIC)
+  uint16_t read_pdm;
+  if (!pdmspi.decimateFilterWord(&read_pdm)) {
+    return; // not ready for data yet!
+  }
+  sample = read_pdm;
+#endif
+
   if (isRecording) {
     digitalWrite(LED_OUT, val);
     val = !val;
-    int16_t sample = analogRead(AUDIO_IN);
+#if defined(USE_EXTERNAL_MIC)
+    sample = analogRead(AUDIO_IN);
     sample -= 2047; // 12 bit audio unsigned  0-4095 to signed -2048-~2047
     sample *= 16;   // convert 12 bit to 16 bit
+#endif
+#if defined(USE_EDGEBADGE_PDMMIC)
+    sample -= 32676;
+    sample *= 2;
+#endif
     audio_buffer[audio_idx] = sample;
     audio_idx++;
     if (audio_idx == BUFFER_SIZE) {
@@ -158,12 +178,18 @@ void loop() {
 }
 
 void initTimer(void) {
+#if defined(USE_EXTERNAL_MIC)
   zt.configure(TC_CLOCK_PRESCALER_DIV1, // prescaler
                 TC_COUNTER_SIZE_16BIT,   // bit width of timer/counter
                 TC_WAVE_GENERATION_MATCH_PWM // frequency or PWM mode
                 );
 
   zt.setCompare(0, (24000000/SAMPLERATE_HZ)*2);
-  zt.setCallback(true, TC_CALLBACK_CC_CHANNEL0, TimerCallback);  // this one sets pin low
+  zt.setCallback(true, TC_CALLBACK_CC_CHANNEL0, TIMER_CALLBACK);  // this one sets pin low
   zt.enable(true);
+#endif
+#if defined(USE_EDGEBADGE_PDMMIC)
+  pdmspi.begin(SAMPLERATE_HZ);
+  Serial.print("Final PDM frequency: "); Serial.println(pdmspi.sampleRate);
+#endif
 }
